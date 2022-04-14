e5dcfbf317968e44c28a6f15d12277c1de2b8a07
Thivi <theviyanthan@outlook.com>
2022-04-14 14:35:54 +0400

    Add get signature api

--- consensus/poi/api.go
+++ consensus/poi/api.go
@@ -1,13 +1,39 @@
 package poi
 
 import (
-	"github.com/ethereum/go-ethereum/log"
+	"encoding/base64"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
 )
 
 type API struct {
 	poi *PoI
 }
 
-func (api *API) Echo(text string) {
-	log.Info(text)
+func (api *API) GetSignature(coinbase string) {
+	resp, err := http.Get("http://localhost:8080/getSignature/" + coinbase)
+	if err != nil {
+		panic(err)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	var sign SignatureResponse
+	err = json.Unmarshal(body, &sign)
+
+	if err != nil {
+		panic(err)
+	}
+
+	signData, err := base64.StdEncoding.DecodeString(sign.Signature)
+
+	if err != nil {
+		panic(err)
+	}
+
+	api.poi.SetSignature(signData)
 }
